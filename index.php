e5bed831a9523274fbb5d697c097ad1aade79ac4
Chris Johnson <crjjrc@gmail.com>
2019-12-07 21:04:48 +0000

    Fix sizing and reference issues on Safari.

--- index.php
+++ index.php
@@ -5,9 +5,9 @@ $html = file_get_contents('index.html');
 if (array_key_exists('src', $_REQUEST)) {
   $src = str_replace(array("\r\n", "\n", "\r"), "\\n", $_REQUEST['src']);
   $src = str_replace("'", "\\'", $src);
-  $script = "source0 = '$src';";
+  $script = "window.source0 = '$src';";
   if (array_key_exists('runZeroMode', $_REQUEST)) {
-    $script .= "\nrunZeroMode = '{$_REQUEST['runZeroMode']}';";
+    $script .= "\nwindow.runZeroMode = '{$_REQUEST['runZeroMode']}';";
   }
   $html = str_replace('// SRC:PHP', $script, $html);
 }
