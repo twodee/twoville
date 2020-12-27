19329ca087caf2fab2fc1cd773e3f5e53a969e2d
Chris Johnson <crjjrc@gmail.com>
2020-12-26 20:54:39 -0500

    Refactor stroke. Nodes hold state, not callbacks.

---
+++ src/stroke.js
@@ -0,0 +1,92 @@
+import {
+  LocatedException,
+} from './common';
+
+import {
+  TimelinedEnvironment
+} from './environment';
+
+import {
+  ExpressionInteger,
+  ExpressionReal,
+} from './ast';
+
+// --------------------------------------------------------------------------- 
+
+export class Stroke extends TimelinedEnvironment {
+  static type = 'stroke';
+  static article = 'a';
+  static timedIds = ['size', 'color', 'opacity', 'dashes', 'join'];
+
+  static create(parentEnvironment, where) {
+    const stroke = new Stroke();
+    stroke.initialize(parentEnvironment, where);
+    return stroke;
+  }
+
+  static reify(parentEnvironment, pod) {
+    const stroke = new Stroke();
+    stroke.embody(parentEnvironment, pod);
+    return stroke;
+  }
+}
+
+// --------------------------------------------------------------------------- 
+
+export function configureStroke(stateHost, domHost, bounds) {
+  // TODO allow not required
+
+  // Stroke color.
+  stateHost.configureVectorProperty('color', stateHost, domHost, domHost.updateStrokeColorDom.bind(domHost), bounds, [], timeline => {
+    if (timeline) {
+      try {
+        timeline.assertList(3, ExpressionInteger, ExpressionReal);
+        return true;
+      } catch (e) {
+        throw new LocatedException(e.where, `I found an illegal value for <code>color</code>. ${e.message}`);
+      }
+    } else {
+      throw new LocatedException(stateHost.where, `I found ${domHost.article} ${domHost.type} whose stroke <code>color</code> isn't set.`);
+    }
+  });
+
+  // Stroke opacity.
+  stateHost.configureScalarProperty('opacity', stateHost, domHost, domHost.updateStrokeOpacityDom.bind(domHost), bounds, [], timeline => {
+    if (timeline) {
+      try {
+        timeline.assertScalar(ExpressionInteger, ExpressionReal);
+        return true;
+      } catch (e) {
+        throw new LocatedException(e.where, `I found an illegal value for <code>opacity</code>. ${e.message}`);
+      }
+    }
+    return true;
+  });
+
+  // Stroke size.
+  stateHost.configureScalarProperty('size', stateHost, domHost, domHost.updateStrokeSizeDom.bind(domHost), bounds, [], timeline => {
+    if (timeline) {
+      try {
+        timeline.assertScalar(ExpressionInteger, ExpressionReal);
+        return true;
+      } catch (e) {
+        throw new LocatedException(e.where, `I found an illegal value for <code>size</code>. ${e.message}`);
+      }
+    }
+    return true;
+  });
+
+  // if (stateHost.owns('dashes')) {
+    // const dashes = stateHost.valueAt(env, 'dashes', t).toSpacedString();
+    // element.setAttributeNS(null, 'stroke-dasharray', dashes);
+  // }
+
+  /*
+
+    if (stateHost.owns('join')) {
+      const type = stateHost.valueAt(env, 'join', t).value;
+      element.setAttributeNS(null, 'stroke-linejoin', type);
+    }
+    */
+}
+
